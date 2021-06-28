dee6b0d25850e36f55d67e6b541bbd53367cf429
Noncenze <thmas.dee@gmail.com>
2021-06-28 16:01:07 -0400

    Added authentication

---
+++ src/utils/setAuthToken.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+// This utility will add the authorized user's JWT to the request header
+// Any routes that are protected will require the JWT in order to access them.
+
+const setAuthToken = (token) => {
+    if (token) {
+        // If true: Apply the token to every request header, especially private routes
+        axios.defaults.headers.common['Authorization'] = token;
+        console.log('----------- HEADERS -----------');
+        console.log(axios.defaults.headers.common);
+    } else {
+        // If false: Remove the authorization
+        delete axios.defaults.headers.common['Authorization']
+    }
+};
+
+export default setAuthToken;
