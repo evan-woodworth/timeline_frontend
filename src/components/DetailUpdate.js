7cc2251a16c7336fc155052792a1d3a0f2605472
Noncenze <thmas.dee@gmail.com>
2021-07-03 18:56:53 -0400

    Entries update functionality

--- src/components/DetailUpdate.js
+++ src/components/DetailUpdate.js
@@ -1,7 +1,11 @@
 import React, {useState} from 'react';
+import axios from 'axios';
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL
+
 
 const DetailUpdate = (props) => {
-    const { id, user, title, summary, description, image } = props.entry
+    console.log(props)
+    const { id, user, timeline, categories, datetime, title, summary, description, image } = props.entry
     const [updateTitle, setUpdateTitle] = useState(title);
     const [updateSummary, setUpdateSummary] = useState(summary);
     const [updateDescription, setUpdateDescription] = useState(description);
@@ -10,6 +14,9 @@ const DetailUpdate = (props) => {
     const data = {
         id: id,
         user: user,
+        timeline: timeline,
+        categories: categories,
+        datetime: datetime,
         title: updateTitle,
         summary: updateSummary,
         description: updateDescription,
@@ -35,16 +42,30 @@ const DetailUpdate = (props) => {
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(data)
+        axios.put(`${REACT_APP_SERVER_URL}/api/entries/${id}/`, data)
+        .then(response => {
+            console.log(response.data);
+            alert(`Entry ${title} updated`);
+        }).catch(error => {
+            console.log('--------- ENTRY UPDATE ERROR ---------')
+            console.log(error);
+        })
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <label htmlFor="title"> Title </label>
             <input type="text" name="title" value={updateTitle} onChange={handleTitle} />
-            
+            <br/>
             <label htmlFor="summary"> Summary </label>
             <input type="text" name="summary" value={updateSummary} onChange={handleSummary} />
-            
+            <br/>
+            <label htmlFor="description"> Description </label>
+            <input type="text" name="description" value={updateDescription} onChange={handleDescription} />
+            <br/>
+            <label htmlFor="image"> Image </label>
+            <input type="text" name="image" value={updateImage} onChange={handleImage} />
+            <br/>
             <button type="submit" className="btn btn-info">Submit</button>
         </form>
     )
