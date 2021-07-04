6ceecdcc903ba41dd0898b93e7940ffe0d1b5f0e
evan-woodworth <evan.woodworth@gmail.com>
2021-07-03 18:43:02 -0700

    entry update and delete accounts for nested entries

--- src/components/DetailShow.js
+++ src/components/DetailShow.js
@@ -12,12 +12,12 @@ export default function DetailShow(props) {
         setUpdatePage(!updatePage)
     }
 
-    const handleDelete = () => {
+    const handleDelete = (e, entryToDelete) => {
         // axios.delete(`${REACT_APP_SERVER_URL}/api/entries/${entry.id}/`, payload)
-        axios.delete(`${REACT_APP_SERVER_URL}/api/entries/${entry.id}/`)
+        axios.delete(`${REACT_APP_SERVER_URL}/api/entries/${entryToDelete.id}/`)
         .then(response => {
             console.log(response.data);
-            alert(`Entry ${entry.title} deleted`);
+            alert(`Entry ${entryToDelete.title} deleted`);
         }).catch(error => {
             console.log('------------ ENTRY DELETE ERROR ------------');
             console.log(error);
@@ -38,18 +38,30 @@ export default function DetailShow(props) {
                         <h5>{nestEntry.date}</h5>
                         <h6>{nestEntry.subject}</h6>
                         <p>{nestEntry.description}</p>
+                        { (updatePage === true) ? (
+                            <div>
+                                <DetailUpdate entry={nestEntry}/>
+                            </div>
+                            
+                        ) : <></> }
+                        <div className="btn btn-secondary" onClick={handleUpdate}> Update </div>
+                        <div className="btn btn-danger" onClick={e=>handleDelete(e,nestEntry)}> Delete </div>
                     </div>
                 ))
             ) : (
-                <p>{entry.description}</p>
-            ) }
-            <div className="btn btn-secondary" onClick={handleUpdate}> Update </div>
-            <div className="btn btn-danger" onClick={handleDelete}> Delete </div>
-            { (updatePage === true) ? (
                 <div>
-                    <DetailUpdate entry={props.entry}/>
+                    <p>{entry.description}</p>
+                    { (updatePage === true) ? (
+                        <div>
+                            <DetailUpdate entry={entry}/>
+                        </div>
+                    ) : <></> }
+                    <div className="btn btn-secondary" onClick={handleUpdate}> Update </div>
+                    <div className="btn btn-danger" onClick={e=>handleDelete(e,entry)}> Delete </div>
                 </div>
-            ) : <></> }
+            )}
+
+
         </div>
     )
 }
